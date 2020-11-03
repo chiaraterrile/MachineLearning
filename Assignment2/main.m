451fab15cdce6cae032018a68121e840438b114a
chiaraterrile <chiaraterrile97@gmail.com>
2020-11-03 12:30:41 +0100

    Concluding Task3

--- Assignment2/main.m
+++ Assignment2/main.m
@@ -7,5 +7,9 @@ turkish = load('turkish-se.txt');
 task2(turkish,mtcars);
 
 %% Task 3
-
-%task3(turkish,mtcars);
+J = ones(6,10);
+for i= 1:10
+J(:,i) = task3(turkish,mtcars,i);
+end
+%table containing the mean square errors for 10 random splits
+T = array2table(J,'VariableNames',{'Attempt1','Attempt2','Attempt3','Attempt4','Attempt5','Attempt6','Attempt7','Attempt8','Attempt9','Attempt10'},'RowNames',{'J_mse_1_5(1D without int)','J_mse_2_5(1D with int)','J_mse_3_5(multidim)','J_mse_1_95(1D without int)','J_mse_2_95(1D with int)','J_mse_3_95(multidim)'});
