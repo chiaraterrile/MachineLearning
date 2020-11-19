c4ebb50113e5a827d17252457688433b5d37afbb
chiaraterrile <chiaraterrile97@gmail.com>
2020-11-19 12:43:38 +0100

    task 1 e 2

--- Assignment3/kNN_Classifier.m
+++ Assignment3/kNN_Classifier.m
@@ -1,4 +1,4 @@
-function [y,distances]=kNN_Classifier(X_Training,T_Training,X_Test,k,T_test)
+function [y]=kNN_Classifier(X_Training,T_Training,X_Test,k,T_test)
 [n,d] = size(X_Training);
 m = size(X_Test,1); 
 %controls on the input values
@@ -11,20 +11,17 @@ end
 if k<0 || k >= n
     error (' the k value is not valid');
 end
-
-
-if nargin == 4
     y = zeros(m,1);
     for z = 1 : m
-    X_bar=X_Test(z,:);
     distances = zeros(n,1);
-    val = 0;
+    
     for i = 1:n
+        val = 0;
         for j = 1:d
-            val = val + (X_Training(i,j)- X_bar(1,j))^2;
+            val = val + (X_Training(i,j)- X_Test(z,j))^2;
         end
-        d = sqrt (val);
-        distances(i,1)=d;
+        val1 = sqrt (val);
+        distances(i,1)=val1;
         
     end
      [ordered_dist,index] = sort(distances);
@@ -32,7 +29,7 @@ if nargin == 4
      k_label = T_Training(k_index);
      y(z,1) = mode (k_label);
     end
-elseif nargin == 5
-        
-end
+
+
+
 end
--- Assignment3/main.m
+++ Assignment3/main.m
@@ -4,42 +4,44 @@
 %% Task 1 : Obtain a data set
 clear all
 close all
-[X_Training,T_Training]= loadMNIST(0);
-[X_Test,T_test]= loadMNIST(1);
+% [X_Training,T_Training]= loadMNIST(0);
+% [X_Test,T_test]= loadMNIST(1);
+X_Training = [ 3 5 9 1; 6 2 9 5 ; 6 2 4  3; 9 2 2 1; 8 7 8 3; 1 4 2 7 ];
+T_Training = [ 1 3 2 1 1 3]';
+X_Test=[ 6 2 9 5 ;1 4 2 7  ;6 2 9 5];
 
+[y]=kNN_Classifier(X_Training,T_Training,X_Test,3);
 %% Task 2 : Build a kNN classifier
-k=4;
-%[y,distances]=kNN_Classifier(X_Training,T_Training,X_Test,3);
-[n,d] = size(X_Training);
-m = size(X_Test,1); 
-%controls on the input values
-if size(T_Training) ~= n
-    error (' the size of the class label in the training set is incorrect');
-end
-if size(X_Test,2) ~= d
-    error (' the size of the test set is incorrect');
-end
-if k<0 || k >= n
-    error (' the k value is not valid');
-end
-
-
-
-    y = zeros(m,1);
-    for z = 1 : m
-    X_bar=X_Test(z,:);
-    distances = zeros(n,1);
-    val = 0;
-    for i = 1:n
-        for j = 1:d
-            val = val + (X_Training(i,j)- X_bar(1,j))^2;
-        end
-        val = sqrt (val);
-        distances(i,1)=val;
-        
-    end
-     [ordered_dist,index] = sort(distances);
-     k_index = index(k,:);
-     k_label = T_Training(k_index);
-     y(z,1) = mode (k_label);
-    end
+[y]=kNN_Classifier(X_Training,T_Training,X_Test,3);
+% k=3;
+% 
+% [n,d] = size(X_Training);
+% m = size(X_Test,1); 
+% %controls on the input values
+% if size(T_Training) ~= n
+%     error (' the size of the class label in the training set is incorrect');
+% end
+% if size(X_Test,2) ~= d
+%     error (' the size of the test set is incorrect');
+% end
+% if k<0 || k >= n
+%     error (' the k value is not valid');
+% end
+%     y = zeros(m,1);
+%     for z = 1 : m
+%     distances = zeros(n,1);
+%     
+%     for i = 1:n
+%         val = 0;
+%         for j = 1:d
+%             val = val + (X_Training(i,j)- X_Test(z,j))^2;
+%         end
+%         val1 = sqrt (val);
+%         distances(i,1)=val1;
+%         
+%     end
+%      [ordered_dist,index] = sort(distances);
+%      k_index = index(k,:);
+%      k_label = T_Training(k_index);
+%      y(z,1) = mode (k_label);
+%     end
