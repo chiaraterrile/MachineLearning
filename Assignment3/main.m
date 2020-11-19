db24bf4fba47ddbb93067dc81f4333c6cb7a0da5
chiaraterrile <chiaraterrile97@gmail.com>
2020-11-19 15:54:29 +0100

    gpu

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -6,6 +6,11 @@ clear all
 close all
 [X_Training,T_Training]= loadMNIST(0);
 [X_Test,T_Test]= loadMNIST(1);
+X_Training=gpuArray(X_Training);
+X_Test=gpuArray(X_Test);
+T_Training = gpuArray(T_Training);
+T_Test = gpuArray(T_Test);
+
 % X_Training = [ 3 5 9 1; 6 2 9 5 ; 6 2 4  3; 9 2 2 1; 8 7 8 3; 1 4 2 7 ];
 % T_Training = [ 1 3 2 1 1 3]';
 % X_Test=[ 6 2 9 5 ;1 4 2 7  ;6 2 9 5];
@@ -15,8 +20,9 @@ close all
 %% Task 2 : Build a kNN classifier
 % [y]=kNN_Classifier(X_Training,T_Training,X_Test,3,T_Test);
 %[y]=kNN_Classifier(X_Training,T_Training,X_Test,3);
-
+% reshape(X_Training(1,:), 28, 28);
+% imshow(reshape(X_Training(1,:), 28, 28))
 %% Task 3:Test the kNN classifier
 
 [T_Training,T_Test] = TaskCreator(T_Training,T_Test,0);
-[y]=kNN_Classifier(X_Training,T_Training,X_Test,3,T_Test);
+[y]=kNN_Classifier(X_Training,T_Training,X_Test,1,T_Test);
