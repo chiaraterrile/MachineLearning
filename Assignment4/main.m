1e117b77537bfdd3ef6851dc7b181991175b6be9
chiaraterrile <chiaraterrile97@gmail.com>
2020-12-01 14:25:42 +0100

    altre cose belle

--- Assignment4/main.m
+++ Assignment4/main.m
@@ -4,12 +4,12 @@ close all
 load('iris.txt')
 %prova = iris(46:55,:);
 [confusion_matrix_perceptron] = perceptron(iris,0.1,2);
-[confusion_matrix_adaline] = adaline(iris,0.01,2);
+[confusion_matrix_adaline] = adaline(iris,0.001,2);
 %% 
 
 [X,T]= loadMNIST(0);
 [T] = TaskCreator(T,2); % puts digit 2 equal to 1 and the remaining equal to -1
 data_set = [ X  T];
 data_set_reduced = data_set(1:150,:);
-%[confusion_matrix] = perceptron(data_set_reduced,0.1,2);
-
+[confusion_matrix_perceptron] = perceptron(data_set_reduced,0.1,2);
+[confusion_matrix_adaline] = adaline(data_set_reduced,0.001,2);
